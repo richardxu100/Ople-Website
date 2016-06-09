6f99eb16a2b8bab3230ec7a5f98eec9965ed6db3
Rich Xu <richardxu100@gmail.com>
2016-06-09 17:08:15 -0400

    Contact Form and Jquery implemented

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,31 @@
-$('.item').click(function() {
-	$('.item').removeClass('active');
-	$(this).addClass('active');
+$(document).ready(function() {
+	$('.item').click(function() {
+		$('.item').removeClass('active');
+		$(this).addClass('active');
+	});
+	
+	$('#contact').click(function() {
+		$(this).removeClass('.item');
+	});
+	
+	$("#products").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".products.info").offset().top
+	    }, 700);
+	});	
+	$("#about").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".about.info").offset().top
+	    }, 700);
+	});	
+	$("#contact").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".contact.info").offset().top
+	    }, 700);
+	});		
+	$("#logo").click(function() {
+		$('html, body').animate({
+			scrollTop: $(".masthead").offset().top
+		}, 700);
+	})
 });
-
-$('#contact').click(function() {
-	$(this).removeClass('.item');
-});
-
