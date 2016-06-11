74fd1ad3d31a1b17c0ebd729402ac4a8d4291e14
Rich Xu <richardxu100@gmail.com>
2016-06-11 17:30:42 -0400

    Made The Site Sweet
    I was in a coding flow for 2 and a half hours. I added a navbar effect,
    added icons, straightened up the sections, and added a services and
    footer section

--- js/myscript.js
+++ js/myscript.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
 			scrollTop: $(".masthead").offset().top
 		}, 700);
 	});	
+	$(".down_arrow").click(function() {
+		$('html, body').animate({
+			scrollTop: $(".products.info").offset().top 
+		}, 700);
+	});
 	//Contact form variables
 	$('#send').click(function() {
 		var name, email, message;
@@ -36,7 +41,16 @@ $(document).ready(function() {
 		message = $('#message').val();
 		$.post("../phpmail/mail.php", name, email, message);
 	});
-	
+	//Navbar effects
+	$(window).scroll(function() {
+	  if ($(this).scrollTop() > 600) {
+	  	$('#navbar').addClass('large inverted');
+	  	$('#logo').innerHTML = 'Ople';
+	  }
+	  else {
+	    $('#navbar').removeClass('large inverted');
+	  }
+	});	
 });
 //Javascript
 
